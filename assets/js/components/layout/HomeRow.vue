be351cf257d8c326af93f10539fa28d0218fb07d
Andrew Jennings <afjennings@gmail.com>
2021-04-07 14:12:48 -0400

    GMX-81: Align home rows vertically

--- assets/js/components/layout/HomeRow.vue
+++ assets/js/components/layout/HomeRow.vue
@@ -2,8 +2,8 @@
   <div @swiped-left="forward()" @swiped-right="back()" class="home-row p-6">
     <div class="text-3xl text-left font-bold pl-22 px-12 pt-4 pb-2">{{ settings.title}}</div>
     <div class="flex flex-row justify-start items-center">
-      <div v-show="allowScrolling">
-        <button @click="back()" class="bg-twitch hover:bg-indigo-400 text-gray-800 font-bold p-2 rounded-sm">
+      <div class="w-2">
+        <button v-show="allowScrolling" @click="back()" class="bg-twitch hover:bg-indigo-400 text-gray-800 font-bold p-2 rounded-sm">
           &lt;
         </button>
       </div>
@@ -16,8 +16,8 @@
           ></channel>
         </div>
       </div>
-      <div v-show="allowScrolling">
-        <button @click="forward()" class="bg-twitch hover:bg-indigo-400 text-gray-800 font-bold p-2 rounded-sm">
+      <div class="w-2">
+        <button v-show="allowScrolling" @click="forward()" class="bg-twitch hover:bg-indigo-400 text-gray-800 font-bold p-2 rounded-sm">
           &gt;
         </button>
       </div>
