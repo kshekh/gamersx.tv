3cf57313b2960a28b6bdbcbaa9041a4285e83b28
Andrew Jennings <afjennings@gmail.com>
2021-04-26 15:36:24 -0400

    GMX-91: Add bitbucket pipeline and CodeDeploy app files

---
+++ scripts/start.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+cd /home/andrew/gamersx-deployed
+ln -s ../deps/vendor vendor
+composer install
---
+++ scripts/stop.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+cd /home/andrew
+rm -rf /gamersx-deployed
+mkdir gamersx-deployed
+# ln -s deps/vendor gamersx-deployed/vendor
+# ln -s deps/node_modules gamersx-deployed/node_modules
