8951e7918178b4588d0215276055b6a5d75c46d9
Andrew Jennings <afjennings@gmail.com>
2021-07-07 11:57:47 -0400

    Update script to keep custom images

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,7 @@ cd /var/www/gamersx/codedeploy
 cp ../deploy/.env* .
 ln -sf ../deploy/vendor .
 ln -sf /var/symfony/themes public/images
+ln -sf /var/symfony/items public/images
 mkdir var
 ln -sf /var/symfony/cache var
 ln -sf /var/symfony/log var
