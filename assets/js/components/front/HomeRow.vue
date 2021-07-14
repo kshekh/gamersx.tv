fd888b32a08026bd87bdab1302eefd03fc5f45ae
Andrew Jennings <afjennings@gmail.com>
2021-07-14 15:21:19 -0400

    GMX-132: Add custom art to regular rows as background frame

--- assets/js/components/front/HomeRow.vue
+++ assets/js/components/front/HomeRow.vue
@@ -1,16 +1,18 @@
 <template>
   <div @swiped-left="forward()" @swiped-right="back()" class="home-row p-6">
     <div class="text-3xl text-left font-bold pl-22 px-12 pt-4 pb-2">{{ settings.title}}</div>
-    <div class="flex flex-row justify-start items-center">
+    <div class="flex flex-row justify-start items-baseline">
       <div class="w-16 h-16 flex-shrink-0 flex-grow-0" @click="first()">
         <img alt="cursor-left" class="cursor-pointer" v-show="allowScrolling && (rowIndex > 0)" src="/images/left-arrow.png" />
       </div>
-      <div ref="channelBox" class="flex flex-row p-5 overflow-hidden">
-        <div ref="channelDivs" v-for="channel in displayChannels">
-          <component
-            :is="channel.componentName"
-            v-bind="channel"
-          ></component>
+      <div ref="channelBox" class="w-full flex flex-row p-5 overflow-hidden">
+        <div class="w-full" ref="channelDivs" v-for="channel in displayChannels">
+          <div v-bind:style="customBg(channel)" class="custom-bg relative" >
+            <component class="absolute bottom-6 inset-x-0"
+              :is="channel.componentName"
+              v-bind="channel"
+            ></component>
+          </div>
         </div>
       </div>
       <div class="w-16 h-16 flex-shrink-0 flex-grow-0" @click="forward()">
@@ -65,7 +67,17 @@ export default {
         // Add one to j because flexbox order should start with 1, not 0
         this.$refs.channelDivs[i].style.order = j + 1;
       }
-    }
+    },
+    customBg: function(channel) {
+      if (channel.customArt) {
+        return {
+          'backgroundImage': 'url(' + channel.customArt + ')',
+          'width': '600px',
+        };
+      } else {
+        return {};
+      }
+    },
   },
   mounted: function() {
     this.displayChannels = this.settings.channels.filter(this.showChannel);
