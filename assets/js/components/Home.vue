8fa143864147765b64de464d4fdd5dd29d3ccc6f
vasiliy.h <menzlags@gmail.com>
2021-09-25 00:11:36 +0300

    tailwind config upd; main bg changed;

--- assets/js/components/Home.vue
+++ assets/js/components/Home.vue
@@ -1,50 +1,41 @@
 <template>
   <div class="text-center mx-auto">
-    <div class="divide-y-4 divide-current my-4 flex-col">
-      <div
-        v-for="row in settings.rows">
-        <component
-          :is="row.componentName"
-          v-bind:settings="row"
-        ></component>
-      </div>
+    <div v-for="row in settings.rows" :key="row.id">
+      <component :is="row.componentName" v-bind:settings="row"></component>
     </div>
   </div>
 </template>
 <script>
-import axios from 'axios';
-import ClassicMd from './front/ClassicMd.vue';
-import FullWidthDescriptive from './front/FullWidthDescriptive.vue';
-import Parallax from './front/Parallax.vue';
-import NumberedRow from './front/NumberedRow.vue';
+import axios from "axios";
+import ClassicMd from "./front/ClassicMd.vue";
+import FullWidthDescriptive from "./front/FullWidthDescriptive.vue";
+import Parallax from "./front/Parallax.vue";
+import NumberedRow from "./front/NumberedRow.vue";
 
 export default {
   components: {
-    'ClassicMd': ClassicMd,
-    'FullWidthDescriptive': FullWidthDescriptive,
-    'Parallax': Parallax,
-    'NumberedRow': NumberedRow,
+    ClassicMd: ClassicMd,
+    FullWidthDescriptive: FullWidthDescriptive,
+    Parallax: Parallax,
+    NumberedRow: NumberedRow
   },
-  data: function()  {
+  data: function() {
     return {
       settings: {
         rows: []
-      },
-    }
+      }
+    };
   },
   mounted: function() {
-    axios
-      .get('/home/api')
-      .then(response => {
-        this.settings = response.data.settings;
-      });
+    axios.get("/home/api").then(response => {
+      this.settings = response.data.settings;
+    });
   }
-}
+};
 
 /** We use this a lot for scrolling because JS % is remainder, not modulo **/
 Number.prototype.mod = function(n) {
   return ((this % n) + n) % n;
-}
+};
 </script>
-<style scoped>
-</style>
+<style scoped></style>
