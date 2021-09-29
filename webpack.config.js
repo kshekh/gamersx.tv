fda9516838a1c249c388845a42fb10eb333699cd
VlsnkVld <menzlags@gmail.com>
2021-09-30 01:10:32 +0300

    add entrypoint;

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('main', './assets/main.js')
 
      /*
       * Vue Apps
