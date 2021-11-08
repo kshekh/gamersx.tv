c5c786394fc606382c114015a9454995254c3963
VlsnkVld <menzlags@gmail.com>
2021-11-08 13:34:52 +0200

    limit the selectable area;

--- assets/js/components/layout/EmbedContainer/EmbedContainerFullWidthDescriptive.vue
+++ assets/js/components/layout/EmbedContainer/EmbedContainerFullWidthDescriptive.vue
@@ -170,7 +170,6 @@
     <!-- Show the embed with overlay if there's an embed -->
     <div
       v-if="showEmbed && embedData"
-      :class="{ 'pointer-events-none': !isHideButtonClicked }"
     >
       <div v-show="isEmbedVisible">
         <component
