7411b00e0bd13f142241d6fffc5e9264d7288010
Dev Pio <developers@programmers.io>
2022-06-15 20:32:44 +0530

    Added JS code to Extract Params From URLs

--- public/js/custom-admin.js
+++ public/js/custom-admin.js
@@ -9,31 +9,77 @@ $(document).ready(function(){
         
         if(typeof(title) != 'undefined'){
             if(title == 'twitch-video'){
-                $('.video-id').val('https://player.twitch.tv/?video=');
+                $('.video-id').val('');
+                $('.video-id').attr('placeholder', 'https://player.twitch.tv/?video=');
                 $('.video-id').parent().parent().removeClass('hide');
                 $('.playlist-id').parent().parent().addClass('hide');
             }
             else if(title == 'twitch-playlist'){
-                $('.playlist-id').val('https://player.twitch.tv/?collection=');
+                $('.playlist-id').val('');
+                $('.playlist-id').attr('placeholder', 'https://player.twitch.tv/?collection=');
                 $('.video-id').parent().parent().addClass('hide');
                 $('.playlist-id').parent().parent().removeClass('hide');
             }
             else if(title == 'youtube-video'){
-                $('.video-id').val('https://www.googleapis.com/youtube/v3//videos?key=');
+                $('.video-id').val('');
+                $('.video-id').attr('placeholder', 'https://www.googleapis.com/youtube/v3//videos?key=');
                 $('.video-id').parent().parent().removeClass('hide');
                 $('.playlist-id').parent().parent().addClass('hide');
             }
             else if(title == 'youtube-playlist'){
-                $('.playlist-id').val('https://www.googleapis.com/youtube/v3/playlists?key=');
+                $('.playlist-id').val('');
+                $('.playlist-id').attr('placeholder', 'https://www.googleapis.com/youtube/v3/playlists?key=');
                 $('.video-id').parent().parent().addClass('hide');
                 $('.playlist-id').parent().parent().removeClass('hide');
             }
             else{
                 $('.video-id').val('');
                 $('.playlist-id').val('');
+                $('.video-id').attr('placeholder', '');
+                $('.playlist-id').attr('placeholder', '');
                 $('.video-id').parent().parent().addClass('hide');
                 $('.playlist-id').parent().parent().addClass('hide');
             }
         }
+    });
+
+    $('.video-id').on('blur', function(){
+        var videoUrl = $('.video-id').val();
+        if(videoUrl && typeof(videoUrl) != 'undefined'){
+            let videoUrlSplit = videoUrl.split('?')[1];
+            
+            const videoUrlParams = new URLSearchParams(videoUrlSplit);
+            const videoId = videoUrlParams.get('video');
+                        
+            if(videoId && typeof(videoId) != 'undefined'){
+                $('.video-id').val(videoId); 
+            }   
+            
+            const videoKey = videoUrlParams.get('key');
+            
+            if(videoKey && typeof(videoKey) != 'undefined'){
+                $('.video-id').val(videoKey); 
+            }
+        }
+    });
+
+    $('.playlist-id').on('blur', function(){
+        var playlistUrl = $('.playlist-id').val();
+        if(playlistUrl && playlistUrl != 'undefined'){
+            let playlistUrlSplit = playlistUrl.split('?')[1];
+
+            const playlistUrlParams = new URLSearchParams(playlistUrlSplit);
+            const collectionId = playlistUrlParams.get('collection');
+                        
+            if(collectionId && typeof(collectionId) != 'undefined'){
+                $('.playlist-id').val(collectionId); 
+            }   
+            
+            const playlistKey = playlistUrlParams.get('key');
+            
+            if(playlistKey && typeof(playlistKey) != 'undefined'){
+                $('.playlist-id').val(playlistKey); 
+            }
+        }        
     });
 });   
