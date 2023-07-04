bf3cd9d2cb061a7c841e481c1b5fc89d15b5dd8a
Ravi Kotwani <ravi.kotwani@nexuslinkservices.in>
2023-07-04 11:07:11 +0000

    Revert "Fixed issue in default saved time conversation (pull request #242)"

--- src/Service/HomeRowInfo.php
+++ src/Service/HomeRowInfo.php
@@ -7,11 +7,6 @@ class HomeRowInfo
     public function convertHoursMinutesToSeconds($time)
     {
         $array = explode(':', $time);
-        if(isset($array[1])){
-            return $array[0] * 3600 + $array[1] * 60;
-        }else{
-            return $time;
-        }
-
+        return $array[0] * 3600 + $array[1] * 60;
     }
 }
