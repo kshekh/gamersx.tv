24b41f7fbb4c758e1e7f1e244489bd272ea560b5
Ravi Kotwani <ravi.kotwani@nexuslinkservices.in>
2023-09-18 17:40:00 +0530

    GMX-346-embed-mobile: embed mobile container changes.

--- assets/js/components/layout/EmbedContainer/EmbedContainerNumbered.vue
+++ assets/js/components/layout/EmbedContainer/EmbedContainerNumbered.vue
@@ -2,6 +2,7 @@
   <div
     class="flex items-center w-18 h-24 md:w-24 md:h-32 xl:w-30 xl:h-40 shrink-0"
     ref="itemWrapper"
+    v-if="!isMobileDevice"
   >
     <div
       @click="isShowTwitchEmbed = true"
@@ -145,6 +146,75 @@
       </div>
     </div>
   </div>
+
+  <div class="flex items-center w-18 h-24 md:w-24 md:h-32 xl:w-30 xl:h-40 shrink-0" v-else>
+    <div
+      @click="isShowTwitchEmbed = true"
+      v-show="!isEmbedVisible"
+      class="cut-edge__wrapper h-full w-50 xs:pr-26 sm:pr-16 lg:pr-0 xl:pr-16 2xl:pr-0 xs:w-75 md:w-75 lg:w-80 xl:w-118 2xl:w-127"
+      style="aspect-ratio: 3/4"
+      :class="getGlow"
+    >
+      <div
+        class="cut-edge__clipped cut-edge__clipped-top-right-md h-full bg-black overflow-hidden"
+        :class="getOutline"
+      >
+        <img
+          v-if="showArt && image"
+          :src="image.url"
+          class="-translate-y-1/2 relative top-1/2 transform w-30p h-full object-cover"
+        />
+
+        <img
+          v-else-if="showOverlay"
+          alt="Embed's Custom Overlay"
+          :src="overlay"
+          class="relative top-1/2 transform -translate-y-1/2 w-full h-full object-cover"
+        />
+        <play-button
+          v-if="showEmbed && embedData"
+          class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 h-12 md:h-16 xl:h-32 w-12 md:w-16 xl:w-32"
+          svgClass="w-3 md:w-7 xl:w-12"
+          wrapperClass="md:pl-1.5 xl:pl-3"
+          :videoType="playBtnColor"
+          @click.native="playVideo"
+        />
+      </div>
+    </div>
+
+    <!-- Show the embed with overlay if there's an embed -->
+    <div v-if="showEmbed && embedData">
+      <div
+        class="cut-edge__wrapper flex-grow min-h-0 absolute inset-0 z-20 py-5 md:py-8 xl:py-12 px-4 md:px-18 xl:px-32 opacity-0 transition-opacity duration-300 ease-linear"
+        :class="[
+          getOutlineBorder,
+          {
+            'opacity-100': isEmbedVisible,
+            'pointer-events-none z-negative': !isEmbedVisible,
+          },
+        ]"
+      >
+        <div ref="embedWrapper" class="h-full w-full">
+          <component
+            v-if="embedData"
+            ref="embed"
+            :is="embedName"
+            :embedData="embedData"
+            :overlay="overlay"
+            :image="image"
+            :isShowTwitchEmbed="isShowTwitchEmbed"
+            class="h-full w-full border overflow-hidden bg-black"
+            :class="{
+              'border-purple': embedName === 'TwitchEmbed',
+              'border-red': embedName === 'YouTubeEmbed',
+            }"
+            :width="'100%'"
+            :height="'100%'"
+          ></component>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -154,6 +224,7 @@ import YouTubeEmbed from "../../embeds/YouTubeEmbed.vue";
 import embedMixin from "../../../mixins/embedFrameMixin";
 
 import PlayButton from "../../helpers/PlayButton.vue";
+import isBoxInViewport from "../../../mixins/isBoxInViewport";
 
 export default {
   name: "EmbedContainerNumbered",
@@ -165,6 +236,8 @@ export default {
   },
   props: [
     "title",
+    "info",
+    "customArt",
     "channelName",
     "showOnline",
     "onlineDisplay",
@@ -184,16 +257,43 @@ export default {
   ],
   data: function () {
     return {
+      isOverlayVisible: true,
+      isEmbedVisible: false,
+      isTitleVisible: false,
       glowStyling: {
         glow: "",
       },
       cornerCutStyling: {
         outline: "",
+        outlineBorder: "",
       },
       isShowTwitchEmbed: false,
+      isMobileDevice: false,
     };
   },
   computed: {
+    playBtnColor() {
+      return this.embedName === "TwitchEmbed" ? "twitch" : "youtube";
+    },
+    showEmbed() {
+      return (
+        (this.showOnline && this.onlineDisplay.showEmbed) ||
+        (!this.showOnline && this.offlineDisplay.showEmbed)
+      );
+    },
+    showArt() {
+      return (
+        (this.showOnline && this.onlineDisplay.showArt) ||
+        (!this.showOnline && this.offlineDisplay.showArt)
+      );
+    },
+    showOverlay() {
+      return (
+        this.overlay &&
+        ((this.showOnline && this.onlineDisplay.showOverlay) ||
+          (!this.showOnline && this.offlineDisplay.showOverlay))
+      );
+    },
     getOutline: function () {
       this.computeGlowStyling();
       return this.cornerCutStyling.outline;
@@ -202,8 +302,31 @@ export default {
       this.computeGlowStyling();
       return this.glowStyling.glow;
     },
+    getOutlineBorder: function () {
+      this.computeGlowStyling();
+      return this.cornerCutStyling.outlineBorder;
+    },
+  },
+  mounted() {
+    this.setIsMobileDevice();
+
+    this.$root.$on("close-other-layouts", this.scrollOut);
+    this.isOverlayVisible = this.showOverlay;
+    this.isEmbedVisible = this.showEmbed && !this.isOverlayVisible;
+  },
+  destroyed() {
+    this.$emit("show-controls");
+    this.$root.$off("close-other-layouts", this.scrollOut);
   },
   methods: {
+    setIsMobileDevice() {
+      const checkDeviceType = navigator.userAgent.toLowerCase().match(/mobile/i);
+      if(checkDeviceType) {
+        this.isMobileDevice = true;
+      } else {
+        this.isMobileDevice = false;
+      }
+    },
     computeGlowStyling: function () {
       if (
         this.isGlowStyling === "always_on" ||
@@ -212,8 +335,10 @@ export default {
       ) {
         if (this.embedName === "TwitchEmbed") {
           this.glowStyling.glow = "cut-edge__wrapper--twitch";
+          this.cornerCutStyling.outlineBorder = "cut-edge__clipped--twitch border-purple";
         } else if (this.embedName === "YouTubeEmbed") {
           this.glowStyling.glow = "cut-edge__wrapper--youtube";
+          this.cornerCutStyling.outlineBorder = "cut-edge__clipped--youtube border-red";
         }
       }
 
@@ -228,6 +353,29 @@ export default {
           this.cornerCutStyling.outline = "cut-edge__clipped--youtube";
         }
       }
+    },
+    playVideo() {
+      this.$root.$emit("close-other-layouts");
+      setTimeout(() => {
+        if (this.showOverlay || this.showArt) {
+          this.isOverlayVisible = false;
+          this.isEmbedVisible = true;
+        }
+      }, 0);
+      window.addEventListener("scroll", this.checkIfBoxInViewPort);
+      this.$refs.embed.startPlayer();
+      this.$emit("hide-controls");
+    },
+    scrollOut() {
+      if (this.showOverlay || this.showArt) {
+        this.isOverlayVisible = true;
+        this.isEmbedVisible = false;
+      }
+      // if (this.$refs.embed.isPlaying()) {
+      this.$refs.embed.stopPlayer();
+      // }
+      window.removeEventListener("scroll", this.checkIfBoxInViewPort);
+      this.$emit("show-controls");
     },
   },
   // created() {
