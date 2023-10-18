c3d9d4287ddbeafff1620be45c4aa5d82c7d3b27
Ravi Kotwani <ravi.kotwani@nexuslinkservices.in>
2023-10-18 16:38:28 +0530

    GMX-346: Added static embed

--- assets/js/components/Home.vue
+++ assets/js/components/Home.vue
@@ -52,6 +52,7 @@
         </div>
       </div>
     </Modal>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/DgGHSMGC2EU?si=VOkTJrRL-qwBDMn7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   </div>
 </template>
 <script>
