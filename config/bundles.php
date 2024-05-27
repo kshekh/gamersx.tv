60e1846c548e6a768388a3110321ae4187a78989
Ahmed Ikram <ikramahmed398@gmail.com>
2024-05-28 01:31:41 +0500

    added sonata-project/exporter

--- config/bundles.php
+++ config/bundles.php
@@ -29,4 +29,5 @@ return [
     Knp\Bundle\GaufretteBundle\KnpGaufretteBundle::class => ['all' => true],
     Sonata\Twig\Bridge\Symfony\SonataTwigBundle::class => ['all' => true],
     Sonata\Doctrine\Bridge\Symfony\SonataDoctrineBundle::class => ['all' => true],
+    Sonata\Exporter\Bridge\Symfony\SonataExporterBundle::class => ['all' => true],
 ];
