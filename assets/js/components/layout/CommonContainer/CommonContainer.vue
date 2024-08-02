302365ec814189d93c3adf4219aa44d33e5524ca
Ahmed Ikram <ikramahmed398@gmail.com>
2024-08-02 10:32:38 +0500

    made style improvements to popover

--- assets/js/components/layout/CommonContainer/CommonContainer.vue
+++ assets/js/components/layout/CommonContainer/CommonContainer.vue
@@ -2,9 +2,9 @@
   <div
     :class="[...classNames]"
     :style="{ zIndex: '1000', ...customStyles }"
-    class="w-[500px] h-[350px] "
+    class="w-[500px] h-[350px] common-container"
   >
-    <div oncontextmenu="false" class="actions--wrapper border-4 border-b-0 shadow-2xl shadow-purple-600 border-purple overflow-hidden">
+    <div oncontextmenu="false" class="actions--wrapper border-4 border-b-0 shadow-2xl shadow-purple-600 border-purple overflow-hidden common-container__actions">
       <div
         @click="(event) => $emit('on-pin', event)"
         :class="['actions--btn', { 'actions--btn-active': isPinActive }]"
@@ -22,8 +22,9 @@
         <CommonContainerIcon :icon-type="'close'" />
       </div>
     </div>
+<!--    bg-black border-3 outline outline-[3px] outline-[#7A4ECC] !shadow-2xl !shadow-purple-600-->
     <div
-      class="w-full h-full p-14 flex flex-col relative rounded-[10px] rounded-tr-none bg-black border-4 !shadow-2xl !shadow-purple-600 border-purple"
+      class="w-full h-full p-14 flex flex-col relative rounded-[10px] rounded-tr-none common-container__body"
       :class="innerWrapperClassNames"
     >
       <slot></slot>
