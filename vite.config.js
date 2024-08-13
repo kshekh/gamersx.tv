3f8f9e05c02b3f63f54ac04fdc27e081fb9ae43d
Ahmed Ikram <ikramahmed398@gmail.com>
2024-08-13 12:14:01 +0500

    fixed some styling issues

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import symfonyPlugin from "vite-plugin-symfony";
 import vuePlugin from "@vitejs/plugin-vue";
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
 
 /* if you're using React */
 // import react from '@vitejs/plugin-react';
@@ -10,32 +10,32 @@ import { fileURLToPath } from 'url';
 const mainDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    plugins: [
-        vuePlugin(),
-        symfonyPlugin({
-          stimulus: true
-        }),
-    ],
-    build: {
-        manifest: true,
-        rollupOptions: {
-            input: {
-                app: "./assets/app.js",
-                main: "./assets/main.js",
-                game: "./assets/js/game.js",
-                home: "./assets/js/home.js",
-                streamer: "./assets/js/streamer.js",
-                streamerSelector: "./assets/js/streamerSelector.js",
-                topicSelector: "./assets/js/topicSelector.js",
+  plugins: [
+    vuePlugin(),
+    symfonyPlugin({
+      stimulus: true,
+    }),
+  ],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: {
+        app: "./assets/app.js",
+        main: "./assets/main.js",
+        game: "./assets/js/game.js",
+        home: "./assets/js/home.js",
+        streamer: "./assets/js/streamer.js",
+        streamerSelector: "./assets/js/streamerSelector.js",
+        topicSelector: "./assets/js/topicSelector.js",
 
-                theme: './assets/styles/theme.css',
-            },
-        }
+        theme: "./assets/styles/app.css",
+      },
     },
-    resolve: {
-      alias: {
-        vue: '@vue/compat',
-        '~': resolve(mainDir, 'assets'),
-      }
+  },
+  resolve: {
+    alias: {
+      vue: "@vue/compat",
+      "~": resolve(mainDir, "assets"),
     },
+  },
 });
